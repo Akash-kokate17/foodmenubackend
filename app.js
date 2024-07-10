397834b69b8233496b001af3a80c3779afb1ee4f
Akash-kokate17 <Akashkokate1717@gmail.com>
2024-07-10 21:42:07 +0530

    commit

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const nodeMailer = require("nodemailer");
 require("dotenv").config();
 const app = express();
 const cors = require("cors");
+const path = require('path');
+
 const favicon = require("serve-favicon");
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 app.use(express.static(path.join(__dirname, "public")));
