a3803ccd28495389ffcd1005438fec3c5a19000b
Akash-kokate17 <Akashkokate1717@gmail.com>
2024-08-04 18:26:11 +0530

    comment dbconnection

--- databaseConnection.js
+++ databaseConnection.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const connectDatabase = async () => {
   try {
     await mongoose.connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
     });
     console.log('Database connected successfully');
   } catch (error) {
